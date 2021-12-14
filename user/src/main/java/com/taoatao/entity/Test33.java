5cefb2345cbfb116c8a1a26e5a2f016eeda2b2d7
tao tao <timary@qq.com>
2021-12-14 14:31:46 +0800

    新增test33实体

---
+++ user/src/main/java/com/taoatao/entity/Test33.java
@@ -0,0 +1,66 @@
+package com.taoatao.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author taoatao
+ * @since 2021-04-21
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class Test33 implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String mobile;
+
+    private String nickName;
+
+    private String uid;
+
+    private LocalDateTime createTime;
+
+    private String salt;
+
+    private Integer state;
+
+    private Integer type;
+
+    private String storeMarkCode;
+
+    private String pwd;
+
+    private String cityCode;
+
+    private String provinceCode;
+
+    private String remark;
+
+    private String remark2;
+
+    private String remark3;
+
+    private String remark4;
+
+    private String remark5;
+    private String reason;
+}
